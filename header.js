4fc05cdf035eb67927132ce8309299be0d7b68fd
Javier <jchicano43@gmail.com>
2024-01-12 19:02:47 +0100

    Header y footer

---
+++ header.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", ()=>{
+    nav.classList.add("visible");
+    console.log(nav)
+})
+cerrar.addEventListener("click", ()=>{
+    nav.classList.remove("visible");
+})
