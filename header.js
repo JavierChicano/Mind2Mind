83ba647160d2ca781fcf6c5f763ad463c6ad25ee
JChicano <jchicano43@gmail.com>
2024-01-18 19:18:51 +0100

    terminado el carrousel del main

--- header.js
+++ header.js
@@ -2,105 +2,105 @@ const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
-abrir.addEventListener("click", ()=>{
-    nav.classList.add("visible");
-    console.log(nav)
-})
-cerrar.addEventListener("click", ()=>{
-    nav.classList.remove("visible");
-})
+abrir.addEventListener("click", () => {
+  nav.classList.add("visible");
+  console.log(nav);
+});
+cerrar.addEventListener("click", () => {
+  nav.classList.remove("visible");
+});
 
 //Ventana modal Registro
-const abrirModal = document.getElementById('openRegistro');
-const contendorModal = document.getElementById('modal-Registro');
-const cerrarModal = document.getElementById('closeRegistro');
-const abrilModalCuenta = document.getElementById('creaCuenta');
+const abrirModal = document.getElementById("openRegistro");
+const contendorModal = document.getElementById("modal-Registro");
+const cerrarModal = document.getElementById("closeRegistro");
+const abrilModalCuenta = document.getElementById("creaCuenta");
 
-abrirModal.addEventListener("click", ()=>{
-    contendorModal2.classList.remove('show');
-    contendorModal.classList.add('show');
+abrirModal.addEventListener("click", () => {
+  contendorModal2.classList.remove("show");
+  contendorModal.classList.add("show");
 });
-abrilModalCuenta.addEventListener("click", ()=>{
-    contendorModal.classList.add('show');
+abrilModalCuenta.addEventListener("click", () => {
+  contendorModal.classList.add("show");
 });
-cerrarModal.addEventListener("click", ()=>{
-    contendorModal.classList.remove('show');
+cerrarModal.addEventListener("click", () => {
+  contendorModal.classList.remove("show");
 });
 
 //Ventana modal LogIn
-const abrirModal2 = document.getElementById('openLogIn');
-const contendorModal2 = document.getElementById('modal-LogIn');
-const cerrarModal2 = document.getElementById('closeLogIn');
+const abrirModal2 = document.getElementById("openLogIn");
+const contendorModal2 = document.getElementById("modal-LogIn");
+const cerrarModal2 = document.getElementById("closeLogIn");
 
-abrirModal2.addEventListener("click", ()=>{
-    contendorModal.classList.remove('show');
-    contendorModal2.classList.add('show');
+abrirModal2.addEventListener("click", () => {
+  contendorModal.classList.remove("show");
+  contendorModal2.classList.add("show");
 });
-cerrarModal2.addEventListener("click", ()=>{
-    contendorModal2.classList.remove('show');
+cerrarModal2.addEventListener("click", () => {
+  contendorModal2.classList.remove("show");
 });
 
 //Intercambio de ventanas
-const abrirLogIn = document.getElementById('link-A-LogIn');
-const abrirRegistro = document.getElementById('link-A-Registro');
+const abrirLogIn = document.getElementById("link-A-LogIn");
+const abrirRegistro = document.getElementById("link-A-Registro");
 
-abrirLogIn.addEventListener("click", ()=>{
-    //Cerrar la pestaña de registro
-    contendorModal.classList.remove('show');
+abrirLogIn.addEventListener("click", () => {
+  //Cerrar la pestaña de registro
+  contendorModal.classList.remove("show");
 
-    //Abrir la pestaña de logIn
-    contendorModal2.classList.add('show');
+  //Abrir la pestaña de logIn
+  contendorModal2.classList.add("show");
 });
 
-abrirRegistro.addEventListener("click", ()=>{
-    //Cerrar la pestaña de LogIn
-    contendorModal2.classList.remove('show');
+abrirRegistro.addEventListener("click", () => {
+  //Cerrar la pestaña de LogIn
+  contendorModal2.classList.remove("show");
 
-    //Abrir la pestaña de Registro
-    contendorModal.classList.add('show');
+  //Abrir la pestaña de Registro
+  contendorModal.classList.add("show");
 });
 
 //Conexion de la BBDD con el index.html
-$(document).ready(function() {
-    $.ajax({
-        url: 'BBDD/BBDD.php',
-        type: 'GET',
-        success: function(response) {
-            console.log(response);
-        },
-        error: function(error) {
-            console.error('Error al ejecutar el código PHP:', error);
-        }
-    });
+$(document).ready(function () {
+  $.ajax({
+    url: "BBDD/BBDD.php",
+    type: "GET",
+    success: function (response) {
+      console.log(response);
+    },
+    error: function (error) {
+      console.error("Error al ejecutar el código PHP:", error);
+    },
+  });
 });
 
 //Carrousel imagemes
 
-const grande = document.querySelector('.contenedorIMG');
-const punto = document.querySelectorAll('.punto');
+const grande = document.querySelector(".contenedorIMG");
+const punto = document.querySelectorAll(".punto");
 let currentIndex = 0;
 
 // Función para cambiar el div según el índice
 function cambiarDiv(index) {
-    let operacion = index * -25;
-    grande.style.transform = `translateX(${operacion}%)`;
+  let operacion = index * -25;
+  grande.style.transform = `translateX(${operacion}%)`;
 
-    punto.forEach((cadaPunto, i) => {
-        punto[i].classList.remove('activo');
-    });
-    punto[index].classList.add('activo');
+  punto.forEach((cadaPunto, i) => {
+    punto[i].classList.remove("activo");
+  });
+  punto[index].classList.add("activo");
 }
 
 // Evento de clic en los puntos
 punto.forEach((cadaPunto, i) => {
-    punto[i].addEventListener('click', () => {
-        currentIndex = i;
-        cambiarDiv(currentIndex);
-    });
+  punto[i].addEventListener("click", () => {
+    currentIndex = i;
+    cambiarDiv(currentIndex);
+  });
 });
 
-// // Cambiar automáticamente cada 5 segundos (ajusta según tus necesidades)
-// setInterval(() => {
-//     currentIndex = (currentIndex + 1) % punto.length;
-//     cambiarDiv(currentIndex);
-// }, 5000);  // Cambia cada 5 segundos (5000 milisegundos)
+// Cambiar automáticamente cada 5 segundos (ajusta según tus necesidades)
+setInterval(() => {
+  currentIndex = (currentIndex + 1) % punto.length;
+  cambiarDiv(currentIndex);
+}, 5000); // Cambia cada 5 segundos (5000 milisegundos)
