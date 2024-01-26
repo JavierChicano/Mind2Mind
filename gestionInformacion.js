858d814502423a817db4df36dbeb3d71d89c74cf
Javier <jchicano43@gmail.com>
2024-01-26 13:54:27 +0100

    validacion formularios

--- gestionInformacion.js
+++ gestionInformacion.js
@@ -1,97 +1,148 @@
-//Validacion de formularios
+//Formularios
+var formRegistro = document.getElementById("formRegistro");
+var formLogIn = document.getElementById("formLogIn");
+var formContacto = document.getElementById("formContacto");
 
-
-
-
-
-
-
-//Formulario registro información
-document.getElementById("formRegistro").addEventListener("submit", function(event) {
-    event.preventDefault(); 
+if (formRegistro) {
+  formRegistro.addEventListener("submit", function (event) {
+    event.preventDefault();
 
     // Obtener los valores de los campos del formulario
     var nombre = document.getElementById("nombreRegistro").value;
     var apellidos = document.getElementById("apellidosRegistro").value;
     var email = document.getElementById("emailRegistro").value;
     var password = document.getElementById("passwordRegistro").value;
-    var aceptoTerminos = document.getElementById("aceptoTerminosRegistro").checked;
+    var displayErrores = document.getElementById("displayErroresRegistro");
 
-    // Puedes hacer lo que quieras con los datos, por ejemplo, imprimirlos en la consola
-    console.log("Nombre:", nombre);
-    console.log("Apellidos:", apellidos);
-    console.log("Correo electrónico:", email);
-    console.log("Contraseña:", password);
-    console.log("Acepto los términos y condiciones:", aceptoTerminos);
+    // Validacion de formularios
+    var textoLetras = /^[A-Za-z]+$/;
 
-    // Aquí podrías enviar los datos a un servidor mediante una solicitud AJAX o realizar otras acciones según tus necesidades
+    if (!textoLetras.test(nombre)) {
+      displayErrores.textContent = "El nombre contiene caracteres invalidos";
+      document.getElementById("nombreRegistro").style.border = "1px solid red";
+      return false;
+    } else {
+      document.getElementById("nombreRegistro").style.border = "";
+      displayErrores.textContent = "";
+    }
+
+    if (!textoLetras.test(apellidos)) {
+      displayErrores.textContent =
+        "Los apellidos contienen caracteres invalidos";
+      document.getElementById("apellidosRegistro").style.border =
+        "1px solid red";
+      return false;
+    } else {
+      document.getElementById("apellidosRegistro").style.border = "";
+      displayErrores.textContent = "";
+    }
+
+    // Validar el formato del correo electrónico
+    var comprobacionCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!comprobacionCorreo.test(email)) {
+      displayErrores.textContent =
+        "El correo electrónico tiene un formato inválido";
+      document.getElementById("emailRegistro").style.border = "1px solid red";
+      return false;
+    } else {
+      document.getElementById("emailRegistro").style.border = "";
+      displayErrores.textContent = "";
+    }
+
+    // Validar la robustez de la contraseña
+    // var comprobacionContraseña = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+
+    // if (!comprobacionContraseña.test(password)) {
+    //     mostrarError(document.getElementById("passwordRegistro"), "La contraseña no cumple con los criterios de seguridad");
+    //     return false;
+    // }
+
+    // Si todas las validaciones pasan, puedes continuar con el envío del formulario
+    console.log("Formulario válido. Proceder con el envío.");
+    return true;
   });
-  
-  function validacion() { 
-    // Comprobacion del nombre vacío
-    if (nombreVacio()) { 
-       alert('[ERROR] El nombre no tiene contenido.')
-       return false
+}
+
+//Formulario login informacion
+
+if (formLogIn) {
+  formLogIn.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    //Obtener valores del formulario
+    var email = document.getElementById("emailLogin").value;
+    var password = document.getElementById("passwordLogin").value;
+    var displayErrores = document.getElementById("displayErroresRegistro");
+
+    // Validar el formato del correo electrónico
+    var comprobacionCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!comprobacionCorreo.test(email)) {
+      console.log("El correo electrónico tiene un formato inválido");
+      return false;
     }
-    if(telefonoVacio()){
-        alert('[ERROR] El telefono no tiene contenido.');
-        return false;
+
+    // Validar la robustez de la contraseña
+    var comprobacionContraseña = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+    if (!comprobacionContraseña.test(password)) {
+      console.log("La contraseña no cumple con los criterios de seguridad");
+      return false;
     }
-    if(telefonoSoloNumeros()){
-        alert('[ERROR] El telefono no tiene numeros.');
-        return false;
+  });
+}
+
+//Formulario login informacion
+if (formContacto) {
+  formContacto.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Obtener los valores de los campos del formulario
+    var nombre = document.getElementById("nombreContacto").value;
+    var apellidos = document.getElementById("apellidosContacto").value;
+    var email = document.getElementById("emailContacto").value;
+    var telefono = document.getElementById("telefonoContacto").value;
+    var displayErrores = document.getElementById("displayErroresRegistro");
+
+    //  var info = document.getElementById("consultaContacto").value;
+
+    // Validacion de formularios
+    var textoLetras = /^[A-Za-z]+$/;
+
+    if (!textoLetras.test(nombre)) {
+      displayErrores.textContent = "El nombre contiene caracteres invalidos";
+      document.getElementById("nombreContacto").style.border = "1px solid red";
+      return false;
+    } else {
+      document.getElementById("nombreContacto").style.border = "";
+      displayErrores.textContent = "";
     }
-    if(!telefonoLongitudCorrecta()){
-        alert('[ERROR] El telefono no tiene la longitud correcta.');
-        return false;
+
+    if (!textoLetras.test(apellidos)) {
+      displayErrores.textContent =
+        "Los apellidos contienen caracteres invalidos";
+      document.getElementById("apellidosContacto").style.border =
+        "1px solid red";
+      return false;
+    } else {
+      document.getElementById("apellidosContacto").style.border = "";
+      displayErrores.textContent = "";
     }
-    if(!dniValido()){
-        alert('[ERROR] El dni no es correcto.');
-        return false;
+
+    // Validar el formato del correo electrónico
+    var comprobacionCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!comprobacionCorreo.test(email)) {
+      console.log("El correo electrónico tiene un formato inválido");
+      return false;
     }
- 
-    return true
- }
- 
- function nombreVacio() { 
- 
-    let valor = document.getElementById('nombre').value
- 
-    if (valor == null || valor.length == 0 || /^\s+$/.test(valor)) {
-       return true
-    } else { 
-       return false
+    // Validar que el teléfono solo contenga números
+    if (!/^\d+$/.test(telefono)) {
+      displayErrores.textContent = "El teléfono contiene caracteres inválidos";
+      document.getElementById("telefonoContacto").style.border =
+        "1px solid red";
+      return false;
+    } else {
+      document.getElementById("telefonoContacto").style.border = "";
+      displayErrores.textContent = "";
     }
- } 
- function telefonoVacio() { 
-    let valor = document.getElementById('telefono').value
-    if (valor == null || valor.length == 0 || /^\s+$/.test(valor)) {
-       return true
-    } else { 
-       return false
-    }
- } 
- function telefonoSoloNumeros(){
-    let valor = document.getElementById('telefono').value
-    if(isNaN(valor)){
-        return true;
-    }else{
-        return false;
-    }
- }
- function telefonoLongitudCorrecta(){
-    let valor = document.getElementById('telefono').value
-    if(/^\d{9}$/.test(valor)){
-        return true;
-    }else{
-        return false;
-    }
- }
-function dniValido(){
-    let valor = document.getElementById('dni').value
-    if(/^\d{8}[A-Z]$/.test(valor)){
-        return true;
-    }else{
-        return false;
-    }
+  });
 }
